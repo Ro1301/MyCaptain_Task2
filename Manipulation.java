653b79d471883ac817b08d2b5fbfb18166c19042
Irudhaya Ronisha I <162875126+Ro1301@users.noreply.github.com>
2026-06-14 09:14:45 +0530

    Create Manipulation.java

---
+++ Manipulation.java
@@ -0,0 +1,9 @@
+package MyCaptain_Task2;
+public class Manipulation{
+  public static int findsecondlargest(int[] arr){
+    return arr[arr.length- 2];
+  }
+  public static int findsecondsmallest(int[] arr){
+    return arr[1];
+  }
+}
