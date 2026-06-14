97d15844dcb0e583036a9b6ed00dc098fbb90f68
Irudhaya Ronisha I <162875126+Ro1301@users.noreply.github.com>
2026-06-14 09:12:03 +0530

    Create Sortingarr.java

---
+++ Sortingarr.java
@@ -0,0 +1,15 @@
+package MyCaptain_Task2;
+import java.util.*;
+public class Sortingarr{
+  public static void bubblesort(int[] arr){
+    for(int i=0;i<n-1;i++){
+      for(int j=0;j<n-i-1;j++){
+        if(arr[j] > arr[j+ 1]){
+          int temp=arr[j];
+          arr[j]=arr[j+ 1];
+          arr[j+ 1]=temp;
+        }
+      }
+    }
+  }
+}
